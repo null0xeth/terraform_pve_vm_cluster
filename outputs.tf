0814f2a2dd138cbb80ce251620e466c3b4db704a
null0xeth <112922737+null0xeth@users.noreply.github.com>
2024-09-09 19:33:33 +0200

    yeet

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,15 @@
 output "server_name" {
-  value = flatten(values(module.servers)[*].name)
+  description = "Map with all server names of the resources we have created"
+  value       = flatten(values(module.servers)[*].name)
 }
 
 output "server_ipv4" {
-  value = flatten(values(module.servers)[*].ipv4)
+  description = "Map with all server ipv4 addresses of the resources we have created"
+  value       = flatten(values(module.servers)[*].ipv4)
 }
 
 output "raw_map" {
+  description = "Nested map containing `name => ipv4` pairs per VM type."
   value = {
     for category, child in module.servers :
     category => zipmap(flatten(child.name), (flatten(child.ipv4)))
--- variables.tf
+++ variables.tf
@@ -1,6 +1,44 @@
-variable "auth_bundle" {}
-variable "resource_tags" {}
-variable "provider_proxmox" {}
-variable "provider_aws" {}
-variable "cluster_spec" {}
-variable "cloud_init_id" {}
+variable "auth_bundle" {
+  description = "Authentication config injected into the virtual machines"
+  sensitive   = true
+  type = object({
+    username = optional(string)
+    password = optional(string)
+    ssh_keys = optional(list(string))
+  })
+}
+
+variable "resource_tags" {
+  description = "List of resource tags to be added to all created resources"
+  type        = list(string)
+  default     = ["terraform", "infrastructure"]
+}
+
+variable "cluster_spec" {
+  description = "Contains the cluster topology and configuration variables passed to the cluster module."
+  type = map(object({
+    component_id   = optional(string)
+    component_size = optional(number, 3)
+    base_id        = optional(number, 300)
+    base_name      = optional(string, "machine")
+
+    config = object({
+      cpu_cores = optional(number, 4)
+      memory    = optional(number, 4096)
+
+      disks = optional(map(object({
+        datastore_id = optional(string)
+        file_id      = optional(string)
+        interface    = optional(string)
+        size         = optional(number)
+        ssd          = optional(bool)
+      })))
+    })
+  }))
+}
+
+variable "cloud_init_id" {
+  description = "Path to a cloud-init yaml file"
+  type        = string
+  default     = ""
+}
